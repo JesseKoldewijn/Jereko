d029d8292e3996848cdd94b2749d985b776dd88f
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2023-11-04 23:04:50 +0100

    fix: added exp sorting

--- src/components/listers/ExperienceLister.tsx
+++ src/components/listers/ExperienceLister.tsx
@@ -11,13 +11,24 @@ const ExperienceLister = async () => {
   return (
     <div className="flex flex-col gap-4">
       {experience && experience.length > 0 ? (
-        experience.flatMap((exp) => {
-          return (
-            <div key={exp.id}>
-              <ExperienceListerItem experience={exp} />
-            </div>
-          );
-        })
+        experience
+          .sort((a, b) => {
+            const aDate = new Date(
+              "01 " + a.start_month + " " + a.start_year,
+            ).getTime();
+            const bDate = new Date(
+              "01 " + b.start_month + " " + b.start_year,
+            ).getTime();
+
+            return bDate - aDate;
+          })
+          .flatMap((exp) => {
+            return (
+              <div key={exp.id}>
+                <ExperienceListerItem experience={exp} />
+              </div>
+            );
+          })
       ) : (
         <>No experiences found</>
       )}
