d32d577210ed00d2a13930ed8490c5e54833a1bb
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2023-11-15 10:21:11 +0100

    chore: deps upgrade

---
+++ drizzle/0000_striped_gravity.sql
@@ -0,0 +1,43 @@
+CREATE TABLE IF NOT EXISTS "events" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" text,
+	"location" text,
+	"description" varchar,
+	"url" text,
+	"url_type" text,
+	"skills" text,
+	"day" text,
+	"month" text,
+	"year" text,
+	"created_at" date DEFAULT now(),
+	"updated_at" date DEFAULT now()
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "experiences" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"job_title" text,
+	"company_name" text,
+	"location" text,
+	"description" varchar,
+	"skills" text,
+	"start_year" text,
+	"start_month" text,
+	"end_year" text,
+	"end_month" text,
+	"created_at" date DEFAULT now(),
+	"updated_at" date DEFAULT now()
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "projects" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" text,
+	"sub_title" text,
+	"description" varchar,
+	"tags" text,
+	"created_at" date DEFAULT now(),
+	"updated_at" date DEFAULT now()
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX IF NOT EXISTS "unique_idx" ON "events" ("name");--> statement-breakpoint
+CREATE UNIQUE INDEX IF NOT EXISTS "unique_idx" ON "experiences" ("job_title");--> statement-breakpoint
+CREATE UNIQUE INDEX IF NOT EXISTS "unique_idx" ON "projects" ("name");
