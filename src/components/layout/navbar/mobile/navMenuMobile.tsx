9aa76ef61d7c76b33357677b72acdcf9815584a5
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2023-11-22 09:45:06 +0100

    fix: small edit on mobile nav flow

--- src/components/layout/navbar/mobile/navMenuMobile.tsx
+++ src/components/layout/navbar/mobile/navMenuMobile.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { X as CloseIcon, List, MenuIcon } from "lucide-react";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import AppIcons from "@/components/icons/AppIcons";
 import { Button } from "@/components/ui/button";
@@ -13,15 +14,37 @@ import { type Socials } from "@/server/db/schemas/socials";
 import { listedEntry, showcaseEntry } from "../navigationMenu";
 
 const NavMenuMobile = ({ socials }: { socials: Socials | null }) => {
+  const pathName = usePathname();
   const [showMenu, setShowMenu] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
 
   const openMenu = () => {
+    menuRef.current!.style.userSelect = "auto";
+    menuRef.current!.style.cursor = "auto";
+    menuRef.current!.style.opacity = "1";
     setShowMenu(true);
   };
 
   const closeMenu = () => {
-    setShowMenu(false);
+    menuRef.current!.style.userSelect = "none";
+    menuRef.current!.style.cursor = "default";
+    menuRef.current!.style.opacity = "0";
+    setTimeout(() => {
+      setShowMenu(false);
+    }, 700);
   };
+
+  useEffect(() => {
+    if (showMenu) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+    }
+  }, [showMenu]);
+
+  useEffect(() => {
+    closeMenu();
+  }, [pathName]);
 
   return (
     <div className="flex max-w-full flex-col items-center justify-center md:hidden">
@@ -38,100 +61,110 @@ const NavMenuMobile = ({ socials }: { socials: Socials | null }) => {
         {showMenu ? <CloseIcon /> : <MenuIcon />}
         <span className="sr-only">Open mobile navigation menu</span>
       </Button>
-      {showMenu && (
-        <div
-          className={cn(
-            "fixed bottom-0 left-0 right-0 top-12 mt-2 flex bg-neutral-300 !bg-opacity-50 opacity-0 transition-opacity duration-1000 dark:bg-neutral-950",
-          )}
-        >
+      <div
+        ref={menuRef}
+        className="transition-opacity duration-1000"
+        style={{
+          opacity: 0,
+          userSelect: "none",
+          cursor: "default",
+        }}
+      >
+        {showMenu && (
           <div
             className={cn(
-              "absolute inset-0 bottom-0 left-0 right-0 top-0 h-full w-auto bg-neutral-950 bg-opacity-90 blur-3xl",
-            )}
-          ></div>
-          <div
-            className={cn(
-              "relative mx-0 my-auto mb-auto mt-8 flex max-h-[calc(100vh-4rem)] w-full flex-col items-center gap-4 overflow-y-auto px-4 pb-8 pt-6",
+              "opacity-1 fixed bottom-0 left-0 right-0 top-12 mt-2 flex bg-neutral-300 !bg-opacity-50 transition-opacity duration-1000 dark:bg-neutral-950",
             )}
           >
-            <strong>General Pages</strong>
-            {listedEntry.links.flatMap((entry) => {
-              const pathName =
-                entry.href.split("/")[1] !== ""
-                  ? entry.href.split("/")[1]
-                  : "home";
-
-              const Icon =
-                Object.entries(AppIcons.internal).find(
-                  (icon) => icon[0] === pathName,
-                )?.[1] ?? List;
-
-              return (
-                <Link
-                  href={entry.href}
-                  key={entry.title}
-                  className="flex w-full items-center justify-center gap-2 rounded-lg border p-2"
-                >
-                  <Icon className="h5" />
-                  {entry.title}
-                </Link>
-              );
-            })}
-            <Link
-              href="https://github.com/JesseKoldewijn/JKinsight"
-              className="flex w-full items-center justify-center gap-2 rounded-lg border p-2"
+            <div
+              className={cn(
+                "absolute inset-0 bottom-0 left-0 right-0 top-0 h-full w-auto bg-neutral-950 bg-opacity-90 blur-3xl",
+              )}
+            ></div>
+            <div
+              className={cn(
+                "relative mx-0 my-auto mb-auto mt-8 flex max-h-[calc(100vh-4rem)] w-full flex-col items-center gap-4 overflow-y-auto px-4 pb-8 pt-6",
+              )}
             >
-              <AppIcons.socials.github className="h-5" />
-              OpenSource
-            </Link>
-            <strong>About Me</strong>
-            {showcaseEntry.links.flatMap((entry) => {
-              const pathName =
-                entry.href.split("/")[2] !== ""
-                  ? entry.href.split("/")[2]
-                  : "root";
+              <strong>General Pages</strong>
+              {listedEntry.links.flatMap((entry) => {
+                const pathName =
+                  entry.href.split("/")[1] !== ""
+                    ? entry.href.split("/")[1]
+                    : "home";
 
-              const Icon =
-                Object.entries(AppIcons.aboutMe).find(
-                  (icon) => icon[0] === pathName,
-                )?.[1] ?? List;
+                const Icon =
+                  Object.entries(AppIcons.internal).find(
+                    (icon) => icon[0] === pathName,
+                  )?.[1] ?? List;
 
-              return (
-                <Link
-                  href={entry.href}
-                  key={entry.title}
-                  className="flex w-full items-center justify-center gap-2 rounded-lg border p-2"
-                >
-                  <Icon className="h-5" />
-                  {entry.title}
-                </Link>
-              );
-            })}
-            {socials && (
-              <>
-                <strong>Socials</strong>
-                {socials.flatMap((social, idx) => {
-                  const Icon =
-                    Object.entries(AppIcons.socials).find(
-                      (icon) => icon[0] === social.platform,
-                    )?.[1] ?? List;
+                return (
+                  <Link
+                    href={entry.href}
+                    key={entry.title}
+                    className="flex w-full items-center justify-center gap-2 rounded-lg border p-2"
+                  >
+                    <Icon className="h5" />
+                    {entry.title}
+                  </Link>
+                );
+              })}
+              <Link
+                href="https://github.com/JesseKoldewijn/JKinsight"
+                className="flex w-full items-center justify-center gap-2 rounded-lg border p-2"
+              >
+                <AppIcons.socials.github className="h-5" />
+                OpenSource
+              </Link>
+              <strong>About Me</strong>
+              {showcaseEntry.links.flatMap((entry) => {
+                const pathName =
+                  entry.href.split("/")[2] !== ""
+                    ? entry.href.split("/")[2]
+                    : "root";
 
-                  return (
-                    <Link
-                      href={social.link ?? "#"}
-                      key={social.label ?? `social-${idx}`}
-                      className="flex w-full items-center justify-center gap-2 rounded-lg border p-2"
-                    >
-                      <Icon className="h-5" />
-                      {social.label}
-                    </Link>
-                  );
-                })}
-              </>
-            )}
+                const Icon =
+                  Object.entries(AppIcons.aboutMe).find(
+                    (icon) => icon[0] === pathName,
+                  )?.[1] ?? List;
+
+                return (
+                  <Link
+                    href={entry.href}
+                    key={entry.title}
+                    className="flex w-full items-center justify-center gap-2 rounded-lg border p-2"
+                  >
+                    <Icon className="h-5" />
+                    {entry.title}
+                  </Link>
+                );
+              })}
+              {socials && (
+                <>
+                  <strong>Socials</strong>
+                  {socials.flatMap((social, idx) => {
+                    const Icon =
+                      Object.entries(AppIcons.socials).find(
+                        (icon) => icon[0] === social.platform,
+                      )?.[1] ?? List;
+
+                    return (
+                      <Link
+                        href={social.link ?? "#"}
+                        key={social.label ?? `social-${idx}`}
+                        className="flex w-full items-center justify-center gap-2 rounded-lg border p-2"
+                      >
+                        <Icon className="h-5" />
+                        {social.label}
+                      </Link>
+                    );
+                  })}
+                </>
+              )}
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
