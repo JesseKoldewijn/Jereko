bdf6fee2536f4fab68939af9b1c8e16eaed78b7d
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2023-12-11 15:51:44 +0100

    fix: small styling fix on offline+404 page & upgrade to react.js `next` tag

--- src/app/offline/page.tsx
+++ src/app/offline/page.tsx
@@ -1,11 +1,14 @@
 const NotFound = () => {
   return (
-    <div className="flex flex-1 flex-col items-center justify-center px-4 py-2">
-      <h1>Offline</h1>
-      <p>
-        You are currently offline. Please check your internet connection and try
-        again. An active internet connection is required to use my website.
-      </p>
+    <div className="mt-20 flex h-full flex-1 flex-col items-center justify-center px-4 py-2">
+      <div className="my-auto flex max-w-lg flex-col gap-6 text-center">
+        <h1 className="text-2xl font-semibold">Offline</h1>
+        <p>
+          You are currently offline. Please check your internet connection and
+          try again. An active internet connection is required to use my
+          website.
+        </p>
+      </div>
     </div>
   );
 };
