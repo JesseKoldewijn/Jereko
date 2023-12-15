c4d496b7d8e974478a61ab25eb8784aef5ba7f06
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2023-12-15 14:42:25 +0100

    debug: checking pathName on offline page in PWA mode - 2

--- src/app/offline/projects/page.tsx
+++ src/app/offline/projects/page.tsx
@@ -2,13 +2,13 @@
 
 import { usePathname } from "next/navigation";
 
-const OfflineRoot = () => {
+const OfflineProjects = () => {
   const pathName = usePathname();
 
   return (
     <div className="mt-20 flex h-full flex-1 flex-col items-center justify-center px-4 py-2">
       <div className="my-auto flex max-w-lg flex-col gap-6 text-center">
-        <h1 className="text-2xl font-semibold">Offline</h1>
+        <h1 className="text-2xl font-semibold">Projects - Offline</h1>
         <p>
           You are currently offline. Please check your internet connection and
           try again. An active internet connection is required to use my
@@ -22,4 +22,4 @@ const OfflineRoot = () => {
   );
 };
 
-export default OfflineRoot;
+export default OfflineProjects;
