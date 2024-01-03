537e45e8be9d813ab93da0972ac0acf92e40ee95
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2024-01-03 13:54:51 +0100

    fix: moved blog pages back to parallel setup for futureProofing

--- src/app/about-me/layout.tsx
+++ src/app/about-me/layout.tsx
@@ -3,16 +3,16 @@ import React from "react";
 import AboutMeHeader from "@/components/page-elements/about-me/page-heading";
 
 const AboutMeLayout = ({
-  routes,
+  route,
 }: {
   children: React.ReactNode;
-  routes: React.ReactNode;
+  route: React.ReactNode;
 }) => {
   return (
     <>
       <AboutMeHeader />
       <div className="mx-4 flex flex-col items-center justify-center gap-8 lg:mx-2 lg:flex-row">
-        {routes}
+        {route}
       </div>
     </>
   );
---
+++ src/app/blog/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BlogLayout = async ({ route }: { route: React.ReactNode }) => {
+  return <>{route}</>;
+};
+
+export default BlogLayout;
