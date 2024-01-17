57bc1c28b4d23636fcb49e757974a408d8fd0470
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2024-01-17 19:51:36 +0100

    feat: added some more extended sentry config

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -2,7 +2,6 @@
 // The config you add here will be used whenever one of the edge features is loaded.
 // Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
@@ -10,6 +9,9 @@ Sentry.init({
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
+
+  // Profiling sample rate is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,8 +1,8 @@
 // This file configures the initialization of Sentry on the server.
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
 import * as Sentry from "@sentry/nextjs";
+import { ProfilingIntegration } from "@sentry/profiling-node";
 
 Sentry.init({
   dsn: "https://6ba8f2825c3c927c7d25f02a4bf3dd36@o4506540526731264.ingest.sentry.io/4506540535250944",
@@ -10,6 +10,15 @@ Sentry.init({
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
 
+  // Profiling sample rate is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
+
+  // This is an optional setting that allows you to sample traces by a given percentage.
+  integrations: [
+    // Add profiling integration to list of integrations
+    new ProfilingIntegration(),
+  ],
 });
