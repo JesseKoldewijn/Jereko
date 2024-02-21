b15db645e4e6ae47174f231fa01b655038243827
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2024-02-21 22:23:08 +0100

    fix: small adjustment of import flow blog

--- src/app/blog/@route/page.tsx
+++ src/app/blog/@route/page.tsx
@@ -1,12 +1,20 @@
 import type { Metadata } from "next";
-import { Suspense } from "react";
 
 import type { Revalidate } from "next/dist/server/lib/revalidate";
 import dynamic from "next/dynamic";
 
-import { RefreshingList } from "@/components/listers/refreshing/Root";
 import Avatar from "@/images/avatar.webp";
 import { getAllWpPosts } from "@/server/actions/wp-fetch";
+
+const RefreshingList = dynamic(
+  () =>
+    import("@/components/listers/refreshing/Root").then(
+      (mod) => mod.RefreshingList,
+    ),
+  {
+    ssr: true,
+  },
+);
 
 const HeroSection = dynamic(
   () => import("@/components/layout/sections/HeroSection"),
@@ -41,17 +49,15 @@ const BlogPage = async () => {
         }}
       />
       <section className="mx-auto flex w-full max-w-lg flex-col items-center text-center">
-        <Suspense>
-          <RefreshingList
-            actionFunction={getAllWpPosts}
-            listerItemName="blog"
-            queryKey={["blog"]}
-            showRefrashedAt={true}
-            emptyListMessage="No blog posts found."
-            className="flex flex-col gap-4 px-4"
-            ssr
-          />
-        </Suspense>
+        <RefreshingList
+          actionFunction={getAllWpPosts}
+          listerItemName="blog"
+          queryKey={["blog"]}
+          showRefrashedAt={true}
+          emptyListMessage="No blog posts found."
+          className="flex flex-col gap-4 px-4"
+          ssr
+        />
       </section>
     </>
   );
