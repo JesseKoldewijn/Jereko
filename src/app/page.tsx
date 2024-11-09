477e626920c080287c00ed060630091a960c72c6
Jesse Koldewijn <jesse.koldewijn@justeattakeaway.com>
2024-11-09 22:16:15 +0100

    fix: added small text fix on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,13 +31,12 @@ const Home = async () => {
       />
       <section className="mx-auto flex w-full max-w-lg flex-col items-center gap-8 px-4 pt-0 text-center md:pt-5 lg:pt-10">
         <h2 className="text-md font-semibold md:text-xl">
-          Thanks for visiting my personal website!
+          Thanks for the visit!
         </h2>
         <p className="text-neutral-600 dark:text-neutral-200">
           This website is used to showcase my projects, experience and
-          volunteering. I also use this website to showcase my skills in web
-          development, events I&apos;ve attended and possibly also a dev blog in
-          the future.
+          volunteering work. I also use this website to showcase my skills in
+          engineering and events I&apos;ve attended.
         </p>
       </section>
       <section className="mx-auto flex w-full max-w-lg flex-col items-center gap-8 px-4 py-20 text-center">
