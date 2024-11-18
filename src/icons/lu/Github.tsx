7af1aab11bd21e37235e8f9116be2a595886f352
Jesse Koldewijn <jesse.koldewijn@justeattakeaway.com>
2024-11-18 23:42:02 +0100

    fix: tweak of props names in icons

--- src/icons/lu/Github.tsx
+++ src/icons/lu/Github.tsx
@@ -7,10 +7,11 @@ export const LuGithub = (props: CommonSvgAttributes) => {
     viewBox: "0 0 24 24",
     fill: "none",
     stroke: "currentColor",
-    "stroke-width": "2",
-    "stroke-linecap": "round",
-    "stroke-linejoin": "round",
-  };
+    strokeWidth: "2",
+    strokeLinecap: "round",
+    strokeLinejoin: "round",
+  } as const;
+
   const _props = {
     ...defaults,
     ...props,
