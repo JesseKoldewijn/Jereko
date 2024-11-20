ca1df57d458f9d13dec79d7c59fec85c6cc586d7
Jesse Koldewijn <jesse.koldewijn@justeattakeaway.com>
2024-11-21 00:04:47 +0100

    feat: sec - added csp + nonce flow setup

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,39 @@
 import { type NextRequest, NextResponse } from "next/server";
 
 export const middleware = (request: NextRequest) => {
-  // Clone the request headers and set a new header `x-hello-from-middleware1`
+  // Clone the request headers
   const requestHeaders = new Headers(request.headers);
+
+  // set csp w/ nonce
+  const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
+
+  // set nonce header
+  requestHeaders.set("x-nonce", nonce);
+
+  const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+    style-src 'self' 'nonce-${nonce}';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+  `;
+  // Replace newline characters and spaces
+  const contentSecurityPolicyHeaderValue = cspHeader
+    .replace(/\s{2,}/g, " ")
+    .trim();
+
+  // set CSP header
+  requestHeaders.set(
+    "Content-Security-Policy",
+    contentSecurityPolicyHeaderValue,
+  );
+
+  // set header for Document Policy
   requestHeaders.set("Document-Policy", "js-profiling");
 
   // You can also set request headers in NextResponse.next
@@ -13,8 +44,14 @@ export const middleware = (request: NextRequest) => {
     },
   });
 
-  // Set a new response header `x-hello-from-middleware2`
+  // Set a new response header for Document Policy
   response.headers.set("Document-Policy", "js-profiling");
+
+  // set CSP header
+  response.headers.set(
+    "Content-Security-Policy",
+    contentSecurityPolicyHeaderValue,
+  );
 
   // return the response
   return response;
