23ac68866410e07ba16e290ddac901a74b4d9857
Jesse Koldewijn <jesse.koldewijn@justeattakeaway.com>
2024-11-28 23:32:49 +0100

    fix: minor tweak of csp - v4

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,6 @@ import "./src/env";
 const cspHeader = `
     default-src 'self' https://vercel.live;
     script-src 'self' 'unsafe-eval' 'unsafe-inline' https://vercel.live;
-    script-src-elem 'self' 'unsafe-eval' 'unsafe-inline' https://vercel.live;
     style-src 'self' 'unsafe-inline';
     worker-src 'self' 'unsafe-inline' 'unsafe-eval' data: blob:;
     img-src 'self' blob: data:;
