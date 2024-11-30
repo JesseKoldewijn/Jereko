68b815d198d4e12b6d471ee8a19ce6d9960be8a2
Jesse Koldewijn <jesse.koldewijn@justeattakeaway.com>
2024-11-30 22:05:30 +0100

    cleanup: removal of clientside sentry calls

--- src/app/globalErrorPage.tsx
+++ src/app/globalErrorPage.tsx
@@ -1,20 +1,14 @@
 "use client";
 
-import * as Sentry from "@sentry/nextjs";
-import { useEffect } from "react";
-
-const GlobalError = ({ error }: { error: Error & { digest?: string } }) => {
-  useEffect(() => {
-    const isLocalhost = window.location.hostname === "localhost";
-    if (!isLocalhost) {
-      Sentry.captureException(error);
-    }
-  }, [error]);
-
+const GlobalError = ({
+  error: _error,
+}: {
+  error: Error & { digest?: string };
+}) => {
   return (
     <div className="mt-20 flex h-full min-h-[65svh] flex-1 flex-col items-center justify-center px-4 py-2">
       <div className="my-auto flex max-w-lg flex-col gap-6 text-center">
-        <h1 className="text-2xl font-semibold">500 | Server Error</h1>
+        <h1 className="text-2xl font-semibold">Oops!</h1>
         <p>Something went wrong. Please try again later.</p>
       </div>
     </div>
